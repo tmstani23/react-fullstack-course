b8198f9c8fb2418b9667f9af1021b2b48a6fc323
tmstani23 <tmstani23@gmail.com>
2020-05-14 15:33:30 -0500

    styles and state update
    Added new folder for styles using reacts style import functionality.  Began adding state and event handlers to header component.  Added detect change, add to counter, and input paste methods.  Demonstrates basic state usage and class component methods.

--- src/components/header.js
+++ src/components/header.js
@@ -23,13 +23,48 @@ import React, {Component} from 'react';
 
 class Header extends Component {
 
+    state = {
+        title: 'The keywords are: ',
+        keywords: '',
+        count: 0
+    }
+
+    //logs pasted input value to console
+    inputPaste(event) {
+        console.log(event.clipboardData.getData('text'));
+    }
+
+    //adds input value to keywords on change
+    inputChange(event) {
+        //console.log(event.target.value);
+        this.setState({
+            keywords: event.target.value
+        })
+    }
+    //Add one to counter
+    addOne() {
+        this.setState({count: this.state.count + 1})
+    }
+
     render() {
 
-      
         return (
             <header>
-                <div className='logo'>Logo</div>
-                <input />
+                <div className='logo' onClick={() => console.log("I was clicked")}>
+                    Logo
+                </div>
+                <input 
+                    onChange = {(event) => this.inputChange(event)}
+                    // calling onChange = inputChange() will call the function directly when the render happens not onChange
+                    // onPaste = {(event) => this.inputPaste(event)}
+                    onPaste = {(event) => this.inputPaste(event)}
+                />
+                <div>{this.state.title}</div>
+                <div>{this.state.keywords}</div>
+                <br />
+                <div>Count: {this.state.count}</div>
+                <div onClick = { () => this.addOne()}>Add one</div>
+
             </header>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import './styles/styles.css';
 
 import Header from './components/header';
 import NewsList from './components/news_list';
