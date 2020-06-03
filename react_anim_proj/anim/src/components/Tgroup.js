6e266f66fc8816697cbd7534f861291a0bb30cdd
tmstani23 <tmstani23@gmail.com>
2020-06-03 15:16:52 -0500

    anim update - 01
    Created basic structure a project demonstrating react and css transitions. Added 3 different transition demonstrations.  One shows basic transition of a single element with a background.  The  second demonstrates using CSS transitions to animate multiple states on button click.  The last is a component that adds and subtracts a list of elements on button click and adds transitions when they are added/removed.

--- react_anim_proj/anim/src/components/Tgroup.js
+++ react_anim_proj/anim/src/components/Tgroup.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import { CSSTransition, TransitionGroup} from 'react-transition-group'
 
 class Slide extends Component{
     state = {
-        items:[]
+        items:[23, 99,32]
     }
     
-    addElements () {
-      
+    showElements () {
+        return this.state.items.map((item, i) => (
+            <CSSTransition
+                classNames='item'
+                timeout={1000}
+                //appear allows default (items) elements to show
+                appear={true}
+                key={i}
+                // active class is added to the node when it's added to the list
+                onEntered={(node) => {
+                    node.classList.add('active')
+                }}
+            >
+                <div className="item">{item}</div>
+            </CSSTransition>
+            
+        ))
     }
 
     generateNumber(){
-       
+       let itemsArr = [
+            // include whatever is in state items before update   
+            ...this.state.items,
+            Math.floor(Math.random()*100) + 1
+       ]
+       this.setState({
+           items: itemsArr
+       })
     }
 
     removeNumber(){
-      
+        let itemsArr = [
+            ...this.state.items,
+        ].splice(1);
+        
+        this.setState({
+            items: itemsArr
+        })
     }
 
     render(){
         return(
             <div>
-                {this.addElements()}
+                {/* component parameter specifies to wrap all elements from the list in divs */}
+                <TransitionGroup component='div' className="list">
+                    {this.showElements()}
+                </TransitionGroup>
+                
              
                 <div className="btns">
                     <div className="btn-add" onClick={()=> this.generateNumber()}>Add Elements</div>
