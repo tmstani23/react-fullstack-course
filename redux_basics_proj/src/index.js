0cf3726ec1ff8bd42caf9df51deccc191797ff0d
tmstani23 <tmstani23@gmail.com>
2020-06-15 08:29:56 -0500

    nba 5 and redux 1
    Finished nba app.  Created skeleton for basic redux learning project and code demonstrations.  Began working with redux store imports and react_redux provider

---
+++ redux_basics_proj/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+    <Provider>
+      <App />
+    </Provider>,
+  document.getElementById('root')
+);
+
