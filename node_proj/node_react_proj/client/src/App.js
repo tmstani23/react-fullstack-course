dc086788bdf8a2d78fe8d6012a4f90d6eac3aa16
tmstani23 <tmstani23@gmail.com>
2020-06-26 14:23:43 -0500

    node 4
    Added new db to test mongo methods.  Added Mongoose.  Added basic mongoose car model and schema.  Added post and get routes to server.js.  Get route sends the list of cars from the db.  Post routes add and remove a car from the db.  App handles getting the data from the backend api and displaying the list of cars.  Also has methods for removing and adding new cars via button click.

--- node_proj/node_react_proj/client/src/App.js
+++ node_proj/node_react_proj/client/src/App.js
@@ -3,15 +3,68 @@ import axios from 'axios';
 
 class App extends Component {
   
+  state = {
+    cars: [],
+  }
+
   componentDidMount() {
-    axios.get('/api/users').then((req, res) => {
-      console.log(req.data);
+    //Update state with list of cars
+    this.getCars();
+  }
+
+  getCars = () => {
+    //get the cars from backend api and update state
+    axios.get('/api/get_cars').then((response) => {
+      this.setState({
+        cars: response.data
+      })
+    })
+  }
+
+  onCarSubmit = () => {
+    let car = {
+      brand: 'Viper',
+      model: 'Dodge',
+      year: 2012,
+      avail: true,
+    }
+    //Post the car object to the backend api
+    axios.post('/api/add_car', car).then((response) => {
+      console.log(response.data)
+    }).then(this.getCars()) //Update the state to render the new car
+  }
+
+  removeCar = () => {
+    //post the brand to remove to the api
+    axios.post('/api/remove_car', {
+      brand: "Viper"
+    }).then((response) => {
+      console.log(response.data);
+      //update state with new cars list
+      this.getCars();
     })
   }
   
   render() {
     return (
-      <div>Hello World</div>
+      <div className='App'> 
+        <h1> Add Car </h1>
+        <button onClick={() => this.onCarSubmit()}>
+          Add car to db
+        </button>
+        <hr/>
+        {/* render each car from the list as a div */}
+        {
+          this.state.cars.map((car, i) => (
+          <div key = {i}>
+            {`${car.brand} - ${car.model}`}
+          </div>  
+          ))
+        }
+        <hr/>
+        <h1> Remove Car </h1>
+        <button onClick={() => this.removeCar()}>Remove</button>
+      </div>
     )
   }
 }
--- node_proj/node_react_proj/server.js
+++ node_proj/node_react_proj/server.js
@@ -1,46 +1,103 @@
 const express = require('express');
 const app = express();
-const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/Mongo_Test_App';
 const port = process.env.PORT || 3001;
 
-app.get('/api/users', (req, res) => {
-    // res.json([
-    //     {
-    //         id: 1,
-    //         username: 'Timothy'
-    //     },
-    //     {
-    //         id: 2,
-    //         username: 'Marcus'
-    //     },
+app.use(bodyParser.json());
 
-    // ])
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+//Once connected mongoose will keep the connection alive
+    //client.close/setTimeout no longer necessary
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//// CREATE MONGOOSE SCHEMA AND MODEL
+
+//Schema for specifying types
+const carSchema = mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    avail: Boolean,
+})
+
+//Model has a name and reference to schema
+const Car = mongoose.model('Car', carSchema);
+
+/////////////////////////////////////////
+
+///////////////Post Routes//////////
+app.post('/api/add_car', (req, res) => {
+    
+    const addCar = new Car({
+        brand: req.body.brand,
+        model: req.body.model,
+        year: req.body.year,
+        avail: req.body.avail,
+    })
+    
+    
+    addCar.save((err, doc) => {
         if(err) {
-            return console.log("Could not connect to db")
+            return console.log(err)
         }
-        console.log(`Connected to mongodb at url: ${url}`);
-        
-        client.db('Cars').collection('items').insertOne({
-            model:"Ford",
-            year: 2017
-        }, (err, res) => {
-            if(err) {
-                return console.log(`MONGO_ERROR: ${err}`)
-            }
-            //console.log(res);
-            console.log(res.ops[0]._id)
-        })
-        setTimeout(() => {
-            client.close();
-        }, 5000)   
-        
-        
-        
+        console.log(doc);
+
+        res.sendStatus(200);
         
     })
 })
+
+app.post('/api/remove_car', (req, res) => {
+    const brand = req.body.brand;
+    
+    //Will only remove a single document
+    Car.findOneAndRemove({brand}, (err, doc) => {
+        if (err) return console.log(err);
+        res.json(doc);
+    })
+    //Removes a single document that matches the input id
+    // Car.findByIdAndRemove({__id: id}, (err, doc) => {
+    //     if (err) return console.log(err);
+    //     res.json(doc);
+    // })
+
+    //Will remove all documents that match the search
+    // Car.remove({brand}, (err, doc) => {
+    //     if (err) return console.log(err);
+    //     res.json(doc);
+    // })
+    
+})
+/////////////////////////////////////
+
+///////////////Get Routes//////////
+app.get('/api/get_cars', (req, res) => {
+    //Different variations on the find method for getting documents
+    // Car.find({brand: 'Maxima', model: "Nissan"}, (err, doc) => {
+    //     if(err) return console.log(err);
+    //     res.json(doc);
+    // })
+    // Car.findOne({brand: 'Maxima'}, (err, doc) => {
+    //     if(err) return console.log(err);
+    //     //Must return as array.  Find one will return a single obj and that will cause the map to fail in render method
+    //     res.json([doc]); 
+    // })
+    // Car.find({brand: 'Ferrari'}, (err, doc) => {
+    //     if(err) return console.log(err);
+    //     res.json(doc);
+    // })
+    Car.find((err, doc) => {
+        if(err) return console.log(err);
+        res.json(doc);
+    })
+})
+
+/////////////////////////////////////
+
 
 app.listen(port);
